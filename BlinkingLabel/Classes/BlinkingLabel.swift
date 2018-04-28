297a18b3d781817ec5639a8cd44c37e71210c554
chenken_chen <chenken_chen@syntrontech.com>
2018-04-29 00:48:39 +0800

    test

--- BlinkingLabel/Classes/BlinkingLabel.swift
+++ BlinkingLabel/Classes/BlinkingLabel.swift
@@ -1,6 +1,6 @@
 public class BlinkingLabel : UILabel {
     public func startBlinking() {
-       print("yaya startBlinking function run")
+       print("yaya startBlinking function run version 0.1.3")
     }
     
     public func stopBlinking() {
